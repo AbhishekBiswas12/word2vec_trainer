398b6388831c682c3764f93e1f4dd53b90508a3b
AbhishekBiswas12 <rohan.abiswas@gmail.com>
2026-07-26 17:07:33 +0530

    Created model, dataset and trainer classes. Trainer is still in progress

---
+++ dataset/.ipynb_checkpoints/DatasetLoader-checkpoint.py
@@ -0,0 +1,78 @@
+from torch.utils.data import IterableDataset
+from torch.utils.data import DataLoader
+import numpy as np
+import math
+from tqdm.auto import tqdm
+
+class DatasetLoader(IterableDataset):
+    def __init__(self, file_path, batch_size, neg_num=5):
+        self.file_path = file_path
+        self.batch_size = batch_size
+        self.neg_num = neg_num
+        with open(file_path) as f:
+            self.length = sum(1 for _ in f) - 1
+        self.neg_dist = np.load('neg_distribution.npy')
+
+    def __len__(self):
+        return self.length * (1+self.neg_num)
+
+    def gen_negatives(self, contexts, targets, batch):
+      negatives = np.random.choice(
+        len(self.neg_dist),
+        size=(len(contexts), self.neg_num),
+        p=self.neg_dist
+      )
+      for n, t, c in zip(negatives, targets, contexts):
+        for i in range(len(n)):
+          while n[i] == t:
+            n[i] = np.random.choice(
+              len(self.neg_dist),
+              p=self.neg_dist
+            )
+        batch.extend(
+            (c, neg, 0) for neg in n
+        )
+      return batch
+
+    def __iter__(self):
+        with open(self.file_path) as f:
+            next(f)  # skip header
+            np.random.seed(42)
+            batch = []
+            targets = []
+            contexts = []
+
+            pbar = tqdm(
+              total=math.ceil(
+                  (self.length * (1 + self.neg_num))/self.batch_size
+              ),
+              desc="Loading batches",
+              unit="batches"
+            )
+
+            for line in f:
+              context, target, label = line.strip().split(',')
+              context = int(context)
+              target = int(target)
+              label = int(label)
+              contexts.append(context)
+              targets.append(target)
+              batch.append((context, target, label))
+              if len(contexts)==10000:
+                batch = self.gen_negatives(contexts, targets, batch)
+                contexts = []
+                targets = []
+                while len(batch) >= self.batch_size:
+                  pbar.update(1)
+                  yield torch.tensor(
+                      batch[:self.batch_size]
+                  )
+
+                  batch = batch[self.batch_size:]
+            if contexts:
+              batch = self.gen_negatives(contexts, targets, batch)
+
+            if batch:
+              pbar.update(1)
+              yield torch.tensor(batch)
+            pbar.close()
---
+++ model/word2vec_ model.py
@@ -0,0 +1,13 @@
+import torch
+
+class Word2Vec(torch.nn.Module):
+  def __init__(self, vocab_size, embedding_dim):
+    super().__init__()
+    self.input_embedding_layer = torch.nn.Embedding(vocab_size, embedding_dim)
+    self.output_embedding_layer = torch.nn.Embedding(vocab_size, embedding_dim)
+
+  def forward(self, x, y):
+    e1 = self.input_embedding_layer(x)
+    e2 = self.output_embedding_layer(y)
+
+    return (e1 * e2).sum(dim=1)
