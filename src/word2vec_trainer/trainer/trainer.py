52e0112baa1332528467dc82cf66d19b3cf937be
AbhishekBiswas12 <rohan.abiswas@gmail.com>
2026-08-12 11:53:57 +0530

    fixing the trainer

--- src/word2vec_trainer/trainer/trainer.py
+++ src/word2vec_trainer/trainer/trainer.py
@@ -4,8 +4,8 @@ import torch.optim as op
 import numpy as np
 import math
 from tqdm.auto import tqdm
-from src.dataset.dataset_loader import DatasetLoader
-from src.model.word2vec_model import Word2Vec
+from word2vec_trainer.dataset.dataset_loader import DatasetLoader
+from word2vec_trainer.model.word2vec_model import Word2Vec
 
 class Trainer:
     def __init__(
